648d3256a5c185ef376d541f30ef4c8602b94784
Omar Faruk <omarbg.bd@gmail.com>
2025-02-25 21:27:40 +0600

    new version - doing something amazing

--- prisma/migrations/20250225060053_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "ChatMessage" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "content" TEXT NOT NULL,
-    "isUser" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
